4debe315c80185589b693d1311f6071d4872c0ab
mufthims <mufthisoft@gmail.com>
2020-09-11 20:36:02 +0530

    Updates

---
+++ precache-manifest.b2f8d937a47b1dbfde57132d4091d02a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "677ab5dd32c3178f67479c90cd820ca6",
+    "url": "/munavverkhan/cripumps1/index.html"
+  },
+  {
+    "revision": "b044e368b0fb4eacc533",
+    "url": "/munavverkhan/cripumps1/static/css/main.9729edfb.chunk.css"
+  },
+  {
+    "revision": "c173d43f9a3be6c80452",
+    "url": "/munavverkhan/cripumps1/static/js/2.b0ad773b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/munavverkhan/cripumps1/static/js/2.b0ad773b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b044e368b0fb4eacc533",
+    "url": "/munavverkhan/cripumps1/static/js/main.7e412f69.chunk.js"
+  },
+  {
+    "revision": "6b66130aa03329fd5a5f",
+    "url": "/munavverkhan/cripumps1/static/js/runtime-main.6d8d276f.js"
+  },
+  {
+    "revision": "29d96123f6fe54ac99593cfc60bbef44",
+    "url": "/munavverkhan/cripumps1/static/media/1.29d96123.png"
+  },
+  {
+    "revision": "57d5c6509602db21e03912c7a89a79e4",
+    "url": "/munavverkhan/cripumps1/static/media/2.57d5c650.png"
+  },
+  {
+    "revision": "b5ddb1417b1786fc22028f4c49e4ffe3",
+    "url": "/munavverkhan/cripumps1/static/media/3.b5ddb141.png"
+  },
+  {
+    "revision": "5762018e4279189fa56715c827648c1c",
+    "url": "/munavverkhan/cripumps1/static/media/logo.5762018e.png"
+  }
+]);
